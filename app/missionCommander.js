44c612d1909278a01e9965b63659245ff78f4f67
gumodi35 <gumodi10@gmail.com>
2022-05-03 17:31:13 -0500

    agragando la clase MissionComander

---
+++ app/missionCommander.js
@@ -0,0 +1,10 @@
+// crea una clase llamada MissionComander, junto a un constructor que reciba un parametro y lo agregue en el atributo name. No olvides exportar esta clase:
+
+class MissionComander {
+    constructor (name) {
+        this.name = name
+    }
+}
+
+// Exportamos nuestra clase
+module.exports = MissionComander
